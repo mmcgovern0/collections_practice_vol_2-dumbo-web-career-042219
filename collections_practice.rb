bd7c6bba5671e4f96df03365cae17659c474e60e
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 15:15:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,7 +52,7 @@ def merge_data(array_1, array_2)
     merged_hash = {}
     array_1.each do |hash_1|
       if hash_1[:name] == name
-        merged_hash = hash_2.merge()
+        merged_hash = hash_2.merge(hash_1)
   
 end
 
