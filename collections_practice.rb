d48f9ededd19b3db0045ec0e5cb4beb56ea5ebcb
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:50:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,7 @@ def begins_with_r(array)
   array.each do |element|
     if element[0] != "r"
       return false
-    if element[0] == "r"
+    elsif element[0] == "r"
       return true
     end
   end
